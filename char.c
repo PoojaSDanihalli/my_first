d7ae6994229342269e085a1a4ec7337311116c65
Pooja <poojashivashankar11@gmail.com>
2019-09-10 16:56:45 +0530

    my life first git commit

---
+++ char.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+char *name[]={"ravi","Ravindra","ravindralala"};
+printf("%s",*name+1);
+
+}
+
---
+++ freem.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void **rrr;
+int main(void)
+{
+
+rrr=malloc(sizeof(void **));
+*rrr=strdup("bbb");
+free(rrr);
+return 0;
+}
